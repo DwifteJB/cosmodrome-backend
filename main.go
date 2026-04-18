aae3aa79d514224a1b988317d53acf6f76f5b346
DwifteJB <dwifte@outlook.com>
2026-04-18 05:23:56 +0100

    feat: get randomc at

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -9,6 +10,25 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 )
+
+func getRandomCat() (string, error) {
+	resp, err := http.Get("https://api.thecatapi.com/v1/images/search")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// get [0].url from the response
+	type catResponse struct {
+		URL string `json:"url"`
+	}
+	var cats []catResponse
+	err = json.NewDecoder(resp.Body).Decode(&cats)
+	if err != nil {
+		return "", err
+	}
+	return cats[0].URL, nil
+}
 
 func main() {
 	// load env vars
@@ -25,6 +45,18 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		catURL, err := getRandomCat()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+
+		w.Write([]byte(fmt.Sprintf(`<img src="%s" alt="Random Cat" style="max-width: 100%%; height: auto;" />`, catURL)))
+	})
 
 	fmt.Printf("listening on :%s\n", port)
 	if err := http.ListenAndServe(":"+port, r); err != nil {
